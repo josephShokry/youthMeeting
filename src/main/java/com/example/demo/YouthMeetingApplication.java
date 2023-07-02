30c8120a93a4059ee5e4437a0cd9b1f55ca6b96c
josephShokry <josephshokry9@gmail.com>
2023-07-02 10:23:54 +0300

    initial commit

---
+++ src/main/java/com/example/demo/YouthMeetingApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouthMeetingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YouthMeetingApplication.class, args);
+	}
+
+}
