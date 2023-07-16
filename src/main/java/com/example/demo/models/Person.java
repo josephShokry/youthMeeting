33d721cd543feb7384151cf03af6d28f3c9741db
Is7ac <108224601+Is7ac@users.noreply.github.com>
2023-07-16 20:41:36 +0300

    annotations P2

--- src/main/java/com/example/demo/models/Person.java
+++ src/main/java/com/example/demo/models/Person.java
@@ -18,10 +18,14 @@ public class Person {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @Column(name = "first_name")
     private String firstName;
+    @Column(name = "last_name")
     private String lastName;
-//    private Timestamp dayOfBirth;
+    //    private Timestamp dayOfBirth;
+    @Column(name = "phone_number")
     private String phoneNumber;
+    @Column(name = "building number")
     private int buildingNumber;
     @ManyToOne(cascade = CascadeType.PERSIST)
     @JoinColumn(referencedColumnName = "id")
--- src/main/java/com/example/demo/models/Youth.java
+++ src/main/java/com/example/demo/models/Youth.java
@@ -13,16 +13,22 @@ import lombok.*;
 @NoArgsConstructor
 @Builder
 public class Youth extends Person{
+    @Column(name = "university")
     private String university;
+    @Column(name = "college")
     private String college;
+    @Column(name = "college_level")
     private int collegeLevel;
+    @Column(name = "grad_level")
     private int gradLevel;
 
     @ManyToOne(cascade = CascadeType.PERSIST)
     @JoinColumn(referencedColumnName = "id")
     @JsonIgnore
     private Family family;
+    @Column(name = "meeting_level")
     private int meetingLevel; // TODO check if we need 2 levels
-//    private Father father;
+    //    private Father father;
+    @Column(name = "notes")
     private String notes;
 }
