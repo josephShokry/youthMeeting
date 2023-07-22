3e78b518b27231de46641e67ea75afce41af74f1
Is7ac <108224601+Is7ac@users.noreply.github.com>
2023-07-22 14:54:34 +0300

    validation annotation

--- src/main/java/com/example/demo/models/DTOs/StreetDTO.java
+++ src/main/java/com/example/demo/models/DTOs/StreetDTO.java
@@ -2,6 +2,7 @@ package com.example.demo.models.DTOs;
 
 import com.example.demo.models.Youth;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
 
 import java.util.List;
 
@@ -9,6 +10,7 @@ public class StreetDTO {
     @JsonProperty("id")
     public Integer id;
     @JsonProperty("streetName")
+    @NotBlank(message = "please specify the street name")
     public String streetName;
     public List<Youth> youthList;
 }
--- src/main/java/com/example/demo/models/DTOs/YouthDTO.java
+++ src/main/java/com/example/demo/models/DTOs/YouthDTO.java
@@ -1,6 +1,8 @@
 package com.example.demo.models.DTOs;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
@@ -13,10 +15,13 @@ public class YouthDTO {
     @JsonProperty("id")
     public Integer id;
     @JsonProperty("firstName")
+    @NotBlank(message = "please specify the first name")
     public String firstName;
     @JsonProperty("lastName")
+    @NotBlank(message = "please specify the last name")
     public String lastName;
     @JsonProperty("phoneNumber")
+    @NotBlank(message = "please specify the phone number")
     public String phoneNumber;
     public String dayOfBirth;
     @JsonProperty("university")
@@ -28,14 +33,18 @@ public class YouthDTO {
     @JsonProperty("gradLevel")
     public Integer gradLevel;
     @JsonProperty("meetingLevel")
+    @Min(1)
     public Integer meetingLevel;
     @JsonProperty("notes")
     public String notes;
     @JsonProperty("familyId")
+    @Min(1)
     public Integer familyId;
     @JsonProperty("streetId")
+    @Min(1)
     public Integer streetId;
     @JsonProperty("areaId")
+    @Min(1)
     public Integer areaId;
     @JsonProperty("buildingNumber")
     public Integer buildingNumber;
