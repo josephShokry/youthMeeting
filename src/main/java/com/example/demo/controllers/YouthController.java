cf0f19402faa812613a84def1073b56c76d16215
josephShokry <josephshokry9@gmail.com>
2023-08-07 12:16:10 +0300

    continue documentation ducumented the youth controller with @Operation annotation only

--- src/main/java/com/example/demo/controllers/YouthController.java
+++ src/main/java/com/example/demo/controllers/YouthController.java
@@ -5,6 +5,7 @@ import com.example.demo.models.DTOs.YouthFiltersDTO;
 import com.example.demo.models.DTOs.YouthIntermediateDTO;
 import com.example.demo.services.YouthServices;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
@@ -24,25 +25,38 @@ public class YouthController {
     @Autowired
     private YouthServices youthServices;
 
+    @Operation(
+            summary = "Use this api to add new youth",
+            description = "To add new youth to the database")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "the youth added suceessfully")
+    })
     @PostMapping("add")
-    @Operation(description = "to add new youth to the database")
-    @ApiResponse(responseCode = "200",useReturnTypeSchema = true,description = "the youth added suceessfully")
     public ResponseEntity<String> addYouth(@Valid @RequestBody YouthDTO youthDTO) {
         youthServices.addYouth(youthDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body("youth added!");
     }
 
+    @Operation(
+            summary = "Use this api to list all youths depending on some filters criteria",
+            description = "To get all the youths stored in the database")
     @PostMapping("get_all")
     public ResponseEntity<Page<YouthIntermediateDTO>> getAll(@Valid @RequestBody(required = false)
                                                              YouthFiltersDTO youthFiltersDTO) {
         return ResponseEntity.status(HttpStatus.OK).body(youthServices.getAll(youthFiltersDTO));
     }
 
+    @Operation(
+            summary = "Use this api to get all the details of a specific youth",
+            description = "Get details of a specific youth")
     @GetMapping("get")
     public ResponseEntity<YouthDTO> getYouth(@RequestParam Integer youthId) {
         return ResponseEntity.status(HttpStatus.OK).body(youthServices.getYouthDtoById(youthId));
     }
 
+    @Operation(
+            summary = "Use this api to edit any detail of a specific youth",
+            description = "Edit the details of a specific youth")
     @PatchMapping("edit")
     public ResponseEntity<String> editYouth(@RequestBody YouthDTO youthDTO) {
         youthServices.editYouth(youthDTO);
